436d7007bec489d2b91283816e8139d7cb5c988e
flinstonedev <flinstonedev@proton.me>
2025-07-04 23:25:26 +0200

    fix auth config

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -8,11 +8,11 @@ const authConfig = {
       // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
       domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
-    },
-    {
-      domain: process.env.CLERK_HOSTNAME,
-      applicationID: "convex"
     }
+    // {
+    //   domain: process.env.CLERK_HOSTNAME,
+    //   applicationID: "convex"
+    // }
   ],
 };
 
